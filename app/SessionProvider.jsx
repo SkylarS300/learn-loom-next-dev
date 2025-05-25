f9d0efcee635753c47b29e494b9cb6e64d17e3b4
Skylar Schulsohn <skylarksky1234@gmail.com>
2025-05-25 15:20:49 -0400

    fixing

---
+++ app/SessionProvider.jsx
@@ -0,0 +1,8 @@
+// app/SessionProvider.jsx
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+export default function Provider({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+// app/layout.js
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Provider from "./SessionProvider"; // 👈 ADD THIS
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider> {/* 👈 WRAP HERE */}
+          {children}
+        </Provider>
       </body>
     </html>
   );
